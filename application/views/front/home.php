52c19e88b81ec72002363ff788e857545d679edd
Ivan-gely <ivan.gely.pro@gmail.com>
2020-11-30 17:29:35 +0100

    changement du message d'accueil

--- application/views/front/home.php
+++ application/views/front/home.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-	<h2 class="text-success">Bienvenue </h2><hr>
+	<h2 class="text-success">Bienvenue sur votre carte en ligne</h2><hr>
 	<br>
 	<div class="d-flex flex-column align-items-center">
 	<a href="front/login" class="btn btn-success btn-lg m-4 p-3 col-6" role="button">Connexion</a>
